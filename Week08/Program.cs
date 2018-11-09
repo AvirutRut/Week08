020bce54fab17cc4aa6fb4b02d31af8cb77ab13a
AvirutRut <60030112@kmitl.ac.th>
2018-11-09 13:48:18 +0700

    Ex 2.2

--- Week08/Program.cs
+++ Week08/Program.cs
@@ -8,26 +8,35 @@ namespace Week08
     {
         static void Main(string[] args)
         {
-            ArrayList list1 = new ArrayList();
-            list1.Add(10);
-            list1.Add(20);
-            list1.Add(30);
-            list1.Add(40);
-            for (int i = 0; i < list1.Count; i++)
+            int d, m;
+            int s, n = 0;
+            string[] day = { "sunday", "monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sat" };
+            int[] monthday = new int[12];
+            monthday[0] = 0;
+            monthday[1] = 3;
+            monthday[2] = 3;
+            monthday[3] = 6;
+            monthday[4] = 1;
+            monthday[5] = 4;
+            monthday[6] = 6;
+            monthday[7] = 2;
+            monthday[8] = 5;
+            monthday[9] = 0;
+            monthday[10] = 3;
+            monthday[11] = 5;
+            Console.Write("Date = ");
+            d = int.Parse(Console.ReadLine());
+            Console.Write("Month = ");
+            m = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 12; i++)
             {
-                Console.WriteLine(list1[i]);
+                if (m == (i + 1))
+                {
+                    s = d + monthday[i] - 1;
+                    n = s % 7;
+                    Console.WriteLine("{0}/{1}/2017 is {2}", d, m, day[n]);
+                }
             }
-            ArrayList list2 = new ArrayList(list1);
-            list2[2] = 55;
-            list2[3] = 66;
-            list2[4] = 77;
-
-            for (int i = 0; i < list2.Count; i++)
-            {
-                Console.WriteLine(list2[i]);
-            }
-            Console.ReadLine();
-
         }
     }
 }
